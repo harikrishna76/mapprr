ba9e584f4507c750d785a04b237b2f0d870bdadb
Hari Krishna <harikrishna.vanjari@gmail.com>
2017-07-18 21:13:42 +0530

    Dummy data in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,121 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+products =  [
+  {
+    "expire_date": "2016-09-05",
+    "name": "Black Jacket",
+    "sku_id": 1234,
+    "categories": [
+      "jackets",
+      "mens-wear"
+    ],
+    "tags": [
+      "black",
+      "jacket",
+      "leather"
+    ],
+    "images": [
+      {
+        "img_path": "http://mapprr.com/images/logo.png"
+      },
+      {
+        "img_path": "http://mapprr.com/images/logo.png"
+      }
+    ],
+    "price": 1000
+  },
+  {
+    "expire_date": "2016-09-05",
+    "name": "Black Jacket",
+    "sku_id": 1235,
+    "categories": [
+      "jackets",
+      "mens-wear"
+    ],
+    "tags": [
+      "black",
+      "jacket",
+      "leather"
+    ],
+    "images": [
+      {
+        "img_path": "http://mapprr.com/images/logo.png"
+      },
+      {
+        "img_path": "http://mapprr.com/images/logo.png"
+      }
+    ],
+    "price": 1000
+  },
+  {
+    "expire_date": "2016-09-05",
+    "name": "Black Jacket",
+    "sku_id": 1236,
+    "categories": [
+      "jackets",
+      "mens-wear"
+    ],
+    "tags": [
+      "black",
+      "jacket",
+      "leather"
+    ],
+    "images": [
+      {
+        "img_path": "http://mapprr.com/images/logo.png"
+      },
+      {
+        "img_path": "http://mapprr.com/images/logo.png"
+      }
+    ],
+    "price": 1000
+  },
+  {
+    "expire_date": "2016-09-05",
+    "name": "Black Jacket",
+    "sku_id": 1237,
+    "categories": [
+      "jackets",
+      "mens-wear"
+    ],
+    "tags": [
+      "black",
+      "jacket",
+      "leather"
+    ],
+    "images": [
+      {
+        "img_path": "http://mapprr.com/images/logo.png"
+      },
+      {
+        "img_path": "http://mapprr.com/images/logo.png"
+      }
+    ],
+    "price": 1000
+  },
+  {
+    "expire_date": "2016-09-05",
+    "name": "Black Jacket",
+    "sku_id": 1238,
+    "categories": [
+      "jackets",
+      "mens-wear"
+    ],
+    "tags": [
+      "black",
+      "jacket",
+      "leather"
+    ],
+    "images": [
+      {
+        "img_path": "http://mapprr.com/images/logo.png"
+      },
+      {
+        "img_path": "http://mapprr.com/images/logo.png"
+      }
+    ],
+    "price": 1000
+  },
+]
+Product.create(products)
